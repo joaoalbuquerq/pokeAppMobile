76bb305be1ede5e9849f30aacfe95b27f2ce450b
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2025-05-11 20:06:23 -0300

    adicionando pagina de pokedex na tab 3

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -7,13 +7,16 @@ import { ExploreContainerComponentModule } from '../explore-container/explore-co
 
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab3PageRoutingModule
+    Tab3PageRoutingModule,
+    HttpClientModule
   ],
   declarations: [Tab3Page]
 })
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab3',
@@ -8,6 +9,41 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  pokemons: any[] = [];
+  limit = 9;
+  offset = 0;
+  totalPokemons = 1302;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadPokemons();
+  }
+
+  loadPokemons() {
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=${this.limit}&offset=${this.offset}`;
+    this.http.get<any>(url).subscribe(response => {
+      this.pokemons = response.results.map((pokemon: any) => {
+        const id = pokemon.url.split('/').filter(Boolean).pop();
+        return {
+          name: pokemon.name,
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+        };
+      });
+    });
+  }
+
+  avancar() {
+    if (this.offset + this.limit < this.totalPokemons) {
+      this.offset += this.limit;
+      this.loadPokemons();
+    }
+  }
+
+  voltar() {
+    if (this.offset >= this.limit) {
+      this.offset -= this.limit;
+      this.loadPokemons();
+    }
+  }
 }
